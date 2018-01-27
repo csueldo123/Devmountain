5e969d9d041bc24ed2de7bc9d698cae89cfd6c8c
Carlos Sueldo <csueldo@outlook.com>
2018-01-27 11:11:29 -0700

    added calc img

--- React/react-intro-class/react-calc-app/src/App.js
+++ React/react-intro-class/react-calc-app/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Calculator from './components/Calculator/Calculator.js';
 
 class App extends Component {
   render() {
     return (
-      
+      <div>
+        <Calculator />
+      </div>
     );
   }
 }
