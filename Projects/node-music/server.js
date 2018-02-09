886a815deb43e3ed2070b2eb53bdfa84ef606b46
Carlos Sueldo <csueldo@outlook.com>
2018-02-08 18:54:20 -0700

    added server and moved app to client folder

---
+++ Projects/node-music/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use( cors() );
+app.use( bodyParser.json() );
+
+const port = process.env.PORT || 8000;
+
+app.listen( port, () =>{ console.log( `Listening on port ${ port }` )});
