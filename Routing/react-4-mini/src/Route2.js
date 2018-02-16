a0ab11a6fc6f16f4cdade8ca5203899ff852a1da
Carlos Sueldo <csueldo@outlook.com>
2018-02-15 20:39:38 -0700

    routes mini project

---
+++ Routing/react-4-mini/src/Route2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Route2 extends Component {
+    render() {
+        return(
+            <div>
+                Route2
+            </div>
+        )
+    }
+}
+
+export default Route2;
---
+++ Routing/react-4-mini/src/Route3.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Route3 extends Component {
+    render() {
+        return(
+            <div>
+                Route3
+            </div>
+        )
+    }
+}
+
+export default Route3;
